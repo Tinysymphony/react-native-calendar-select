7ccb783726b90f219a54505c8f6d25f82272b57c
ahmad <ahmad2smile@gmail.com>
2018-01-25 12:33:45 +0500

    fixed selectionType prop typo from oneOfType to oneOf

--- Calendar.js
+++ Calendar.js
@@ -25,7 +25,7 @@ export default class Calendar extends Component {
 		}),
 		minDate      : PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
 		maxDate      : PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
-		selectionType: PropTypes.oneOfType(["manual", "week", "day"])
+		selectionType: PropTypes.oneOf(["manual", "week", "day"])
 	}
 	static defaultProps = {
 		format       : "YYYY-MM-DD",
